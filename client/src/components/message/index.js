ac1531d57aa5fc2d587e357420e85e0f4e4fa052
Roman <destabiliseus@gmail.com>
2020-03-19 18:50:37 +0800

    Refactored message

--- client/src/components/message/index.js
+++ client/src/components/message/index.js
@@ -8,8 +8,13 @@ export default class Message extends Component {
       <div className="p-2">
         <div className="flex items-center">
           <div className="w-10 h-10 rounded-full bg-gray-900 flex-shrink-0" />
-          <div className="ml-3">
-            <div className="text-sm text-blue-700 font-bold">{`${message.user.firstName} ${message.user.lastName}`}</div>
+          <div className="flex-grow ml-3">
+            <div className="flex items-center">
+              <div className="flex-grow text-sm text-blue-700 font-bold">
+                {`${message.user.firstName} ${message.user.lastName}`}
+              </div>
+              <div className="text-xs text-gray-600">18:03</div>
+            </div>
             <div>{message.text}</div>
           </div>
         </div>
