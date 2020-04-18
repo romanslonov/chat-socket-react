e247221ad7f76411383b0ff01b0afce9a09812ed
Roman Slonov <destabiliseus@gmail.com>
2020-04-18 19:35:46 +0800

    Fixed emitting user status

--- server/src/socket/user.ts
+++ server/src/socket/user.ts
@@ -1,13 +1,27 @@
-const userStatusChange = (socket, { userId, time }) => {
-  socket.broadcast.emit('USER_STATUS_CHANGE', { userId, time });
+import { connections } from '../socket/connection';
+
+const userStatusChange = (socket, { userId, time }, io) => {
+  const receviers: number[] = socket.user.channels
+	.map((channel) => channel.users)
+  .reduce((acc, users) => acc.concat(users))
+  .map(user => user.id)
+  .filter(id => id !== socket.user.id);
+  const rcvs: number[] = [...new Set(receviers)];
+
+  const sockets = rcvs.map(id => connections.get(id));
+
+  if (sockets.length > 0) {
+    console.log(sockets[0].id);
+    sockets.forEach((socket) => io.to(`${socket.id}`).emit('USER_STATUS_CHANGE', { userId, time }));
+  }
 }
 
 export default function (socket, io) {
   socket.on('USER_ONLINE', (payload) => {
-    userStatusChange(socket, payload);
+    userStatusChange(socket, payload, io);
   });
 
   socket.on('USER_OFFLINE', (payload) => {
-    userStatusChange(socket, payload);
+    userStatusChange(socket, payload, io);
   });
 };
